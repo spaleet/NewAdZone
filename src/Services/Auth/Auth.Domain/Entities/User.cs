7b71a2144b9ed723bc8d62717a478eb52403c45f
Ali M <ali.mo.dev@gmail.com>
2023-03-23 23:38:28 +0430

    created basic User Entity

---
+++ src/Services/Auth/Auth.Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace Auth.Domain.Entities;
+
+public class User : IdentityUser<Guid>
+{
+    public User()
+    {
+
+    }
+
+    [Display(Name = "نام")]
+    [Required]
+    public string FirstName { get; set; }
+
+    [Display(Name = "نام خانوادگی")]
+    [Required]
+    public string LastName { get; set; }
+
+    [Display(Name = "بیوگرافی")]
+    [MaxLength(250)]
+    [MinLength(25)]
+    public string Bio { get; set; }
+
+    [Display(Name = "آواتار")]
+    [Required]
+    public string Avatar { get; set; }
+
+}
