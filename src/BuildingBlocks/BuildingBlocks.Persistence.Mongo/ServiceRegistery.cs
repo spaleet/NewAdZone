7a2dc74ad1366f8b42e332608f97929bdc6cd984
Ali M <ali.mo.dev@gmail.com>
2023-06-16 00:38:57 +0330

    added vasic ServiceRegistery

---
+++ src/BuildingBlocks/BuildingBlocks.Persistence.Mongo/ServiceRegistery.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.Core.Web.Extenions;
+using BuildingBlocks.Persistence.Mongo.Base;
+using BuildingBlocks.Persistence.Mongo.DbContext;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Persistence.Mongo;
+public static class ServiceRegistery
+{
+    public static WebApplicationBuilder AddMongoDbContext<TContext>(this WebApplicationBuilder builder)
+        where TContext : MongoDbContext, IMongoDbContext
+    {
+        var serilogOptions = builder.Configuration.BindOptions<MongoOptions>(nameof(MongoOptions));
+
+        builder.Services.AddScoped(typeof(TContext));
+        builder.Services.AddScoped<IMongoDbContext>(sp => sp.GetRequiredService<TContext>());
+
+        return builder;
+    }
+}
+
