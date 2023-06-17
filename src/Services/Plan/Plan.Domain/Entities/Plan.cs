9a37fe59039d580190933b64463d8a85378f4b92
Ali M <ali.mo.dev@gmail.com>
2023-06-17 19:30:54 +0330

    created Plan Entities

---
+++ src/Services/Plan/Plan.Domain/Entities/Plan.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BuildingBlocks.Persistence.Mongo.Base;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Plan.Domain.Entities;
+
+public class Plan : MongoEntityBase
+{
+    [Display(Name = "نام پلن")]
+    [BsonElement("userId")]
+    [Required]
+    public string Title { get; set; }
+
+    [Display(Name = "تعداد آگهی های قابل بارگزاری")]
+    [BsonElement("adQuota")]
+    public int AdQuota { get; set; }
+
+    [Display(Name = "قیمت پلن")]
+    [BsonElement("price")]
+    public decimal? Price { get; set; }
+
+    [Display(Name = "حساب")]
+    [BsonElement("userId")]
+    public string UserId { get; set; }
+}
