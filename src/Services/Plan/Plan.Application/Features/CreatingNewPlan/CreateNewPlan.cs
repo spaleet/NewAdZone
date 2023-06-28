571d315bffe544f36a4a0d9ebbac691e4eb70f8c
Ali M <ali.mo.dev@gmail.com>
2023-06-28 20:32:58 +0330

    implemented CreateNewPlan

---
+++ src/Services/Plan/Plan.Application/Features/CreatingNewPlan/CreateNewPlan.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using BuildingBlocks.Core.CQRS.Commands;
+using FluentValidation;
+using MediatR;
+using Plan.Infrastructure.Context;
+
+namespace Plan.Application.Features.CreatingNewPlan;
+
+public record CreateNewPlan(string Title, int AdQuota, decimal? Price) : ICommand;
+
+public class CreateNewPlanValidator : AbstractValidator<CreateNewPlan>
+{
+    public CreateNewPlanValidator()
+    {
+        RuleFor(x => x.Title).NotEmpty().WithMessage("عنوان را وارد کنید");
+        RuleFor(x => x.AdQuota)
+            .NotEmpty().NotNull().WithMessage("کوتا را وارد کنید")
+            .NotEqual(0).WithMessage("کوتا معتبر وارد کنید");
+
+        RuleFor(x => x.Price)
+            .Must(x =>
+            {
+                if (x is not null)
+                    if (x < 0 || x == 0) 
+                        return false;
+
+                return true;
+            }).WithMessage("قیمت معتبر وارد کنید");
+    }
+}
+
+public class CreateNewPlanHandler : ICommandHandler<CreateNewPlan>
+{
+    private readonly PlanDbContext _context;
+    private readonly IMapper _mapper;
+
+    public CreateNewPlanHandler(PlanDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(CreateNewPlan request, CancellationToken cancellationToken)
+    {
+        var plan = _mapper.Map(request, new Domain.Entities.Plan());
+
+        await _context.Plans.InsertOneAsync(plan);
+
+        return Unit.Value;
+    }
+}
---
+++ src/Services/Plan/Plan.Application/Features/CreatingNewPlan/CreateNewPlanRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Plan.Application.Features.CreatingNewPlan;
+
+public class CreateNewPlanRequest
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("adQuota")]
+    public int AdQuota { get; set; }
+
+    [Display(Name = "قیمت پلن")]
+    [JsonPropertyName("price")]
+    public decimal? Price { get; set; }
+}
