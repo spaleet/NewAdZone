35f2e92687d8edcac96c64318a3c95dec8421483
Ali M <ali.mo.dev@gmail.com>
2023-07-01 17:31:55 +0330

    implemented InitializePaymentHandler

--- src/Services/Plan/Plan.Application/Features/InitializingPayment/InitializePayment.cs
+++ src/Services/Plan/Plan.Application/Features/InitializingPayment/InitializePayment.cs
@@ -1,3 +1,53 @@
-﻿namespace Plan.Application.Features.InitializingPayment;
+﻿using System.Globalization;
+using AutoMapper;
+using BuildingBlocks.Core.CQRS.Commands;
+using BuildingBlocks.Core.Exceptions.Base;
+using BuildingBlocks.Payment;
+using FluentValidation;
+using MediatR;
+using MongoDB.Driver;
+using Plan.Application.Features.CreatingNewPlan;
+using Plan.Application.Features.SubscribingPlan;
+using Plan.Infrastructure.Context;
 
-public record InitializePayment(string SubId, decimal Price, string CallBackUrl);
+namespace Plan.Application.Features.InitializingPayment;
+
+public record InitializePayment(string SubId, decimal Price, string CallBackUrl) : ICommand<InitializePaymentResponse>;
+
+public class InitializePaymentValidator : AbstractValidator<InitializePayment>
+{
+    public InitializePaymentValidator()
+    {
+        RuleFor(x => x.SubId).NotEmpty().WithMessage("آیدی را وارد کنید");
+        RuleFor(x => x.CallBackUrl).NotEmpty().WithMessage("آدرس برگشت را وارد کنید");
+    }
+}
+
+public class InitializePaymentHandler : ICommandHandler<InitializePayment, InitializePaymentResponse>
+{
+    private readonly PlanDbContext _context;
+    private readonly IZarinPalFactory _zarinPalFactory;
+
+    public InitializePaymentHandler(PlanDbContext context, IZarinPalFactory zarinPalFactory)
+    {
+        _context = context;
+        _zarinPalFactory = zarinPalFactory;
+    }
+
+    public async Task<InitializePaymentResponse> Handle(InitializePayment request, CancellationToken cancellationToken)
+    {
+        var planSub = _context.PlanSubscription.AsQueryable().FirstOrDefault(x => x.Id == request.SubId);
+
+        if (planSub is null)
+            throw new NotFoundException("اشتراک مورد نظر پیدا نشد");
+
+        var paymentResponse = await _zarinPalFactory.CreatePaymentRequest(request.CallBackUrl,
+                                                                          request.Price.ToString(CultureInfo.InvariantCulture),
+                                                                          string.Empty,
+                                                                          planSub.Id);
+
+        string redirectUrl = "https://sandbox.zarinpal.com/pg/StartPay/" + paymentResponse.Authority;
+
+        return new InitializePaymentResponse(redirectUrl);
+    }
+}
