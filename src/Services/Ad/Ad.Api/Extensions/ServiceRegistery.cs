1bb8fb45256b9fab7b705a4de22875f99445f50d
Ali M <ali.mo.dev@gmail.com>
2023-07-21 00:20:17 +0330

    init Ad.Api

---
+++ src/Services/Ad/Ad.Api/Extensions/ServiceRegistery.cs
@@ -0,0 +1,10 @@
+﻿namespace Ad.Api.Extensions;
+
+public static class ServiceRegistery
+{
+    public static void ConfigureSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
--- src/Services/Ad/Ad.Api/Program.cs
+++ src/Services/Ad/Ad.Api/Program.cs
@@ -1,6 +1,28 @@
+using Ad.Api.Extensions;
+using BuildingBlocks.Core.Web;
+using BuildingBlocks.Logging;
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddCustomSerilog();
+builder.AddGeneralConfiguration();
+
+builder.Services.ConfigureSwagger();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseProblemDetails();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
