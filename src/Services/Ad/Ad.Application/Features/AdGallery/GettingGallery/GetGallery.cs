1fdc536eb30bccb9502ef0a46ec6d188ce63a44b
Ali M <ali.mo.dev@gmail.com>
2023-08-14 04:51:52 +0330

    implemented GetGallery

---
+++ src/Services/Ad/Ad.Application/Features/AdGallery/GettingGallery/GetGallery.cs
@@ -0,0 +1,38 @@
+﻿using Ad.Application.Interfaces;
+using BuildingBlocks.Core.CQRS.Queries;
+using BuildingBlocks.Core.Exceptions.Base;
+using FluentValidation;
+
+namespace Ad.Application.Features.AdGallery.GettingGallery;
+
+public record GetGallery(Guid Id) : IQuery<GetGalleryResponse>;
+
+public class GetGalleryValidator : AbstractValidator<GetGallery>
+{
+    public GetGalleryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("لطفا شناسه وارد کنید");
+    }
+}
+
+public class GetGalleryHandler : IQueryHandler<GetGallery, GetGalleryResponse>
+{
+    private readonly IAdDbContext _context;
+
+    public GetGalleryHandler(IAdDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetGalleryResponse> Handle(GetGallery request, CancellationToken cancellationToken)
+    {
+        var gallery = await _context.AdGalleries.FindAsync(request.Id);
+
+        if (gallery is null)
+            throw new NotFoundException("گالری پیدا نشد");
+
+        return new GetGalleryResponse(gallery.ImageSrc); 
+    }
+}
