c5da81e79a7479905310d66919f46b11c15c755c
Ali M <ali.mo.dev@gmail.com>
2023-08-14 04:07:50 +0330

    implemented RemoveGallery

---
+++ src/Services/Ad/Ad.Api/Controllers/GalleryController.cs
@@ -0,0 +1,16 @@
+﻿using Ad.Application.Features.AdCategory.CreatingAdCategory;
+using Ad.Application.Features.AdGallery.UploadingGallery;
+using BuildingBlocks.Core.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ad.Api.Controllers;
+
+public class GalleryController : BaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> Upload([FromForm] UploadGallery request, CancellationToken cancellationToken)
+    {
+        await Mediator.Send(request, cancellationToken);
+        return Ok();
+    }
+}
---
+++ src/Services/Ad/Ad.Application/Features/AdGallery/RemovingGallery/RemoveGallery.cs
@@ -0,0 +1,49 @@
+﻿using Ad.Application.Interfaces;
+using BuildingBlocks.Core.CQRS.Commands;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using BuildingBlocks.Core.Utilities.ImageRelated;
+using BuildingBlocks.Core.Exceptions.Base;
+using Ad.Application.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ad.Application.Features.AdGallery.RemovingGallery;
+
+public record RemoveGallery(string Id) : ICommand;
+
+public class RemoveGalleryValidator : AbstractValidator<RemoveGallery>
+{
+    public RemoveGalleryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("لطفا شناسه وارد کنید");
+    }
+}
+
+public class RemoveGalleryHandler : ICommandHandler<RemoveGallery>
+{
+    private readonly IAdDbContext _context;
+
+    public RemoveGalleryHandler(IAdDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(RemoveGallery request, CancellationToken cancellationToken)
+    {
+        var galleryId = Guid.Parse(request.Id);
+        var gallery = await _context.AdGalleries.FindAsync(galleryId);
+
+        if (gallery is null)
+            throw new NotFoundException("گالری پیدا نشد");
+
+        ImageHelper.DeleteImage(gallery.ImageSrc, "wwwroot/upload/ad_gallery/");
+
+        _context.AdGalleries.Remove(gallery);
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+}
