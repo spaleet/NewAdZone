3e4ec1ec67e56e15d1bc9415576232b4467f6475
Ali M <ali.mo.dev@gmail.com>
2023-08-17 00:19:14 +0330

    implemented AddMessaging

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/ServiceRegistery.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Messaging;
+
+public static class ServiceRegistery
+{
+    public static IServiceCollection AddMessaging(this IServiceCollection services, string connection)
+    {
+        services.AddMassTransit(config =>
+        {
+            config.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(connection);
+            });
+        });
+
+        return services;
+    }
+}
