5b9382e087edbd589b6c3a96a454af3c68ee41ee
Ali M <ali.mo.dev@gmail.com>
2023-08-18 03:07:26 +0330

    added Image Max Size

---
+++ src/BuildingBlocks/BuildingBlocks.Core/Utilities/ImageRelated/MaxSize.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BuildingBlocks.Core.Utilities.ImageRelated;
+
+public static class MaxFileSizeValidator
+{
+    public static bool IsValid(int maxFileSize, object value, bool isRequired = true)
+    {
+        var file = value as IFormFile;
+
+        if (!isRequired && file is null)
+            return true;
+
+        if (file is null)
+            return false;
+
+        return file.Length <= maxFileSize;
+    }
+}
