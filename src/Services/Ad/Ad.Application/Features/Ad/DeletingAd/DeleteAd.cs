78d0cde6f3284705bc676baf81c0e4ce2cf570f9
Ali M <ali.mo.dev@gmail.com>
2023-08-17 03:33:44 +0330

    implemented DeleteAd

---
+++ src/Services/Ad/Ad.Application/Features/Ad/DeletingAd/DeleteAd.cs
@@ -0,0 +1,38 @@
+﻿namespace Ad.Application.Features.Ad.DeletingAd;
+
+public record DeleteAd(long Id) : ICommand;
+
+public class DeleteAdValidator : AbstractValidator<DeleteAd>
+{
+    public DeleteAdValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty()
+            .WithMessage("شناسه را وارد کنید");
+    }
+}
+
+public class DeleteAdHandler : ICommandHandler<DeleteAd>
+{
+    private readonly IAdDbContext _context;
+    public DeleteAdHandler(IAdDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(DeleteAd request, CancellationToken cancellationToken)
+    {
+        // get ad model from db
+        var adModel = await _context.Ads.FindAsync(request.Id);
+
+        // check for null
+        AdNotFoundException.ThrowIfNull(adModel);
+
+        adModel.IsDelete = true;
+
+        _context.Ads.Update(adModel);
+        await _context.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+}
