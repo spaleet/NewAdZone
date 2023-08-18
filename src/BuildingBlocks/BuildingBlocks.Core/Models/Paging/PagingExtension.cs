9b0375795a2a649100e3b818ffdb5e8817fb7e6f
Ali M <ali.mo.dev@gmail.com>
2023-08-19 01:02:51 +0330

    created PagingModel

---
+++ src/BuildingBlocks/BuildingBlocks.Core/Models/Paging/PagingExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BuildingBlocks.Core.Models.Paging;
+
+public static class PagingExtension
+{
+    public static async Task<PagingModel<T>> ApplyPagingAsync<T>(
+        this IQueryable<T> collection,
+        int page = 1,
+        int pageSize = 10,
+        CancellationToken cancellationToken = default
+    )
+        where T : notnull
+    {
+        if (page <= 0)
+            page = 1;
+
+        if (pageSize <= 0)
+            pageSize = 10;
+
+        var isEmpty = await collection.AnyAsync(cancellationToken: cancellationToken) == false;
+        if (isEmpty)
+            return PagingModel<T>.Empty;
+
+        var totalItems = await collection.CountAsync(cancellationToken: cancellationToken);
+        var totalPages = (int)Math.Ceiling((decimal)totalItems / pageSize);
+        var data = await collection.Limit(page, pageSize).ToListAsync(cancellationToken: cancellationToken);
+
+        return PagingModel<T>.Create(data, totalItems, page, pageSize);
+    }
+}
---
+++ src/BuildingBlocks/BuildingBlocks.Core/Models/Paging/PagingModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BuildingBlocks.Core.Models.Paging;
+
+public class PagingModel<T>
+{
+    public PagingModel(List<T> items, long totalItems, int page, int pageSize)
+    {
+        Items = items;
+        TotalItems = totalItems;
+        Page = page;
+        PageSize = pageSize;
+    }
+
+    public int Page { get; set; }
+
+    public int PageSize { get; set; }
+
+    [BindNever]
+    public List<T> Items { get; set; }
+
+    [BindNever]
+    public long TotalItems { get; set; }
+
+    public PagingDataSortCreationDateOrder SortDateOrder { get; set; } = PagingDataSortCreationDateOrder.DES;
+
+    public PagingDataSortIdOrder SortIdOrder { get; set; } = PagingDataSortIdOrder.NotSelected;
+
+
+    //================================== Methods
+    public static PagingModel<T> Empty => new(Enumerable.Empty<T>().ToList(), 0, 0, 0);
+    
+    public static PagingModel<T> Create(List<T> items, long totalItems = 0, int page = 1, int pageSize = 20)
+    {
+        return new PagingModel<T>(items, totalItems, page, pageSize);
+    }
+}
+
+public enum PagingDataSortIdOrder
+{
+    NotSelected,
+    DES,
+    ASC
+}
+
+public enum PagingDataSortCreationDateOrder
+{
+    DES,
+    ASC
+}
