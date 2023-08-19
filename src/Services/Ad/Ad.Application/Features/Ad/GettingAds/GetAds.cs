5e83b1c6ad828fab297f8e94252d89cbbc51e72e
Ali M <ali.mo.dev@gmail.com>
2023-08-19 11:07:45 +0330

    added Title search

--- src/Services/Ad/Ad.Application/Features/Ad/GettingAds/GetAds.cs
+++ src/Services/Ad/Ad.Application/Features/Ad/GettingAds/GetAds.cs
@@ -6,7 +6,7 @@ using BuildingBlocks.Core.Models.Paging;
 
 namespace Ad.Application.Features.Ad.GettingAds;
 
-public record GetAds : PagingQuery<GetAdsResponse>;
+public record GetAds(string? Search) : PagingQuery<GetAdsResponse>;
 
 public class GetAdsValidator : AbstractValidator<GetAds>
 {
@@ -28,10 +28,18 @@ public class GetAdsHandler : IQueryHandler<GetAds, GetAdsResponse>
 
     public async Task<GetAdsResponse> Handle(GetAds request, CancellationToken cancellationToken)
     {
-        var ads = await _context.Ads.IgnoreAutoIncludes()
-                .OrderByDescending(x => x.CreateDate)
-                .AsNoTracking()
-                .ApplyPagingAsync<Domain.Entities.Ad, AdDto>(_mapper.ConfigurationProvider, request.Page, request.PageSize);
+        var query = _context.Ads.IgnoreAutoIncludes().OrderByDescending(x => x.CreateDate)
+                                             .AsNoTracking().AsQueryable();
+
+        // search by title
+        if (!string.IsNullOrEmpty(request.Search))
+            query = query.Where(x => x.Title.Contains(request.Search) || x.Tags.Contains(request.Search));
+
+        // apply paging
+        var ads = await query.ApplyPagingAsync<Domain.Entities.Ad, AdDto>(
+                                                                          _mapper.ConfigurationProvider,
+                                                                          request.Page,
+                                                                          request.PageSize);
 
         return new GetAdsResponse(ads);
     }
