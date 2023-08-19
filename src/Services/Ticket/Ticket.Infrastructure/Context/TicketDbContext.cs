7e8a1bb76f70dc9c97cb96da4906d7e38ae7f856
Ali M <ali.mo.dev@gmail.com>
2023-08-19 11:35:42 +0330

    created TicketDbContext

---
+++ src/Services/Ticket/Ticket.Infrastructure/Context/TicketDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using BuildingBlocks.Persistence.Mongo.Base;
+using BuildingBlocks.Persistence.Mongo.DbContext;
+using Humanizer;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Ticket.Domain.Entities;
+
+namespace Ticket.Infrastructure.Context;
+
+public class TicketDbContext : MongoDbContext
+{
+    public TicketDbContext(IOptions<MongoOptions> options) : base(options.Value)
+    {
+        Tickets = GetCollection<Domain.Entities.Ticket>(nameof(Tickets).Underscore());
+        TicketMessages = GetCollection<TicketMessage>(nameof(TicketMessages).Underscore());
+    }
+
+    public IMongoCollection<Domain.Entities.Ticket> Tickets { get; }
+    public IMongoCollection<TicketMessage> TicketMessages { get; }
+}
