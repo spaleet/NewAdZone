1fc16ebdfd56bb90d0ca93e79cacf9bba242f86b
Ali M <ali.mo.dev@gmail.com>
2023-08-21 01:20:27 +0330

    implemented PostMessage

---
+++ src/Services/Ticket/Ticket.Application/Exceptions/InvalidTicketException.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticket.Application.Exceptions;
+
+public class InvalidTicketException : BadRequestException
+{
+    public InvalidTicketException()
+        : base("تیکت مورد نظر نامعتبر است.")
+    {
+    }
+}
---
+++ src/Services/Ticket/Ticket.Application/Features/User/PostingMessage/PostMessage.cs
@@ -0,0 +1,56 @@
+﻿using MongoDB.Driver;
+using Ticket.Application.Exceptions;
+using Ticket.Domain.Enums;
+
+namespace Ticket.Application.Features.User.PostingMessage;
+public record PostMessage(string UserId, string TicketId, string Text) : ICommand;
+
+public class PostMessageValidator : AbstractValidator<PostMessage>
+{
+    public PostMessageValidator()
+    {
+        RuleFor(x => x.UserId)
+            .RequiredValidator("شناسه کاربری");
+
+        RuleFor(x => x.TicketId)
+            .RequiredValidator("شناسه");
+
+        RuleFor(x => x.Text)
+            .RequiredValidator("متن")
+            .MinLengthValidator("متن", 50)
+            .MaxLengthValidator("متن", 1000);
+    }
+}
+
+public class PostMessageHandler : ICommandHandler<PostMessage>
+{
+    private readonly TicketDbContext _context;
+    private readonly IMapper _mapper;
+
+    public PostMessageHandler(TicketDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(PostMessage request, CancellationToken cancellationToken)
+    {
+        var ticket = _context.Tickets.AsQueryable().FirstOrDefault(x => x.Id == request.TicketId);
+
+
+        if (ticket is null || ticket.UserId != request.UserId)
+            throw new InvalidTicketException();
+
+        // insert ticket message
+        var ticketMessage = new TicketMessage
+        {
+            TicketId = ticket.Id,
+            Text = request.Text,
+            UserId = request.UserId
+        };
+
+        await _context.TicketMessages.InsertOneAsync(ticketMessage);
+
+        return Unit.Value;
+    }
+}
