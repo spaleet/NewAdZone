fe8424f10b9cfb70d325fb1a8c285a3f308acafd
Ali M <ali.mo.dev@gmail.com>
2023-08-22 03:25:55 +0330

    implemented PlanClient

---
+++ src/Services/Ad/Ad.Infrastructure/Clients/PlanClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Ad.Application.Clients;
+using Ad.Infrastructure.Settings;
+using MediatR;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
+
+namespace Ad.Infrastructure.Clients;
+public class PlanClient : IPlanClient
+{
+    private readonly HttpClient _client;
+    private readonly UserClientOptions _settings;
+
+    public PlanClient(HttpClient httpClient, UserClientOptions settings)
+    {
+        _client = httpClient;
+        _settings = settings;
+    }
+
+    public async Task<bool> VerifyPlanLimit(string userId)
+    {
+        if (string.IsNullOrEmpty(userId))
+            throw new ArgumentNullException(nameof(userId));
+
+        var query = new Dictionary<string, string>()
+        {
+            ["uid"] = userId
+        };
+
+        string uri = QueryHelpers.AddQueryString("", query);
+
+        var res = await _client.GetAsync(uri);
+
+        return res.StatusCode == System.Net.HttpStatusCode.OK;
+    }
+}
