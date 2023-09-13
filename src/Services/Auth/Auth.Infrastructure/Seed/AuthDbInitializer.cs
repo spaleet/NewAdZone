1e0bdc8dbea9866d6f010686bfde750cc9234017
Ali M <ali.mo.dev@gmail.com>
2023-09-13 16:55:15 +0330

    auth seeds

--- src/Services/Auth/Auth.Infrastructure/Seed/AuthDbInitializer.cs
+++ src/Services/Auth/Auth.Infrastructure/Seed/AuthDbInitializer.cs
@@ -1,6 +1,7 @@
 ﻿using Auth.Domain.Entities;
 using Auth.Domain.Enums;
 using Auth.Infrastructure.Context;
+using BuildingBlocks.Core.Utilities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -10,13 +11,15 @@ public class AuthDbInitializer
 {
     private readonly DatabaseContext _context;
     private readonly ILogger<AuthDbInitializer> _logger;
+    private readonly UserManager<User> _userManager;
     private readonly RoleManager<UserRole> _roleManager;
 
     public AuthDbInitializer(ILogger<AuthDbInitializer> logger, DatabaseContext context,
-        RoleManager<UserRole> roleManager)
+        UserManager<User> userManager, RoleManager<UserRole> roleManager)
     {
         _logger = logger;
         _context = context;
+        _userManager = userManager;
         _roleManager = roleManager;
     }
 
@@ -28,6 +31,9 @@ public class AuthDbInitializer
             {
                 await _context.Database.MigrateAsync();
             }
+
+            await SeedRolesAsync();
+            await SeedUsersAsync();
         }
         catch (Exception ex)
         {
@@ -36,20 +42,7 @@ public class AuthDbInitializer
         }
     }
 
-    public async Task SeedAsync()
-    {
-        try
-        {
-            await TrySeedAsync();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError("An error occurred while seeding the database : {ex}", ex.Message);
-            throw;
-        }
-    }
-
-    private async Task TrySeedAsync()
+    private async Task SeedRolesAsync()
     {
         if (!await _roleManager.RoleExistsAsync(Roles.Admin.ToString()))
         {
@@ -76,6 +69,58 @@ public class AuthDbInitializer
                 Name = nameof(Roles.BasicUser),
                 NormalizedName = nameof(Roles.BasicUser).ToUpper()
             });
+        }
+    }
+
+    private async Task SeedUsersAsync()
+    {
+        if (!await _userManager.Users.AnyAsync())
+        {
+            //--------- Admin User
+            var adminUser = new User
+            {
+                UserName = "admin",
+                Email = "admin@gmail.com",
+                FirstName = "ادمین",
+                LastName = "ادمینی",
+                PhoneNumber = "09123456789",
+                EmailConfirmed = true
+            };
+
+            await _userManager.CreateAsync(adminUser, "123Pa$$word!");
+            await _userManager.AddToRoleAsync(adminUser, Roles.Admin.ToString());
+            await _userManager.AddToRoleAsync(adminUser, Roles.VerifiedUser.ToString());
+            await _userManager.AddToRoleAsync(adminUser, Roles.BasicUser.ToString());
+
+            //--------- Verified User
+            var verifiedUser = new User
+            {
+                UserName = "cooluser",
+                Email = "cooluser@gmail.com",
+                FirstName = "کاربر فعال",
+                LastName = "کاربری",
+                PhoneNumber = "09987654321",
+                EmailConfirmed = true
+            };
+
+            await _userManager.CreateAsync(verifiedUser, "123Pa$$word!");
+            await _userManager.AddToRoleAsync(verifiedUser, Roles.VerifiedUser.ToString());
+            await _userManager.AddToRoleAsync(verifiedUser, Roles.BasicUser.ToString());
+
+
+            //--------- Basic User
+            var basicUser = new User
+            {
+                UserName = "newuser",
+                Email = "newuser@gmail.com",
+                FirstName = "کاربر",
+                LastName = "کاربری",
+                PhoneNumber = "09321651789",
+                EmailConfirmed = true
+            };
+
+            await _userManager.CreateAsync(basicUser, "123Pa$$word!");
+            await _userManager.AddToRoleAsync(basicUser, Roles.BasicUser.ToString());
         }
     }
 }
